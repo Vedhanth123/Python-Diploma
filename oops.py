e9dbb07bb15a58cc64446599cf58386b79a23c6f
Vedhanth123 <gvedhanth123@outlook.com>
2022-01-07 12:15:30 +0530

    assert and type congestion in python

--- oops.py
+++ oops.py
@@ -1,27 +1,38 @@
+# declaring a class which stores goods stored in an electronics store
 
-class A:
+class Goods():
 
-    classvar1 = "I am a class variable in class A"
+    '''
+    Each good has attributes of:
+        1) name
+        2) price
+        3) quantity
+    '''
 
-    def __init__(self):
+    # creating a constructor
+    def __init__(self, name: str, price: float, quantity: int): # name: str denotes that name parameter given should only contain str other than that datatype will not be accepted, similar to type congestion in C++, Java
 
-        self.var1 = "I am inside class A's constructor"
-        self.classvar1 = "Instance var in class A"
-        self.special = "Special"
-
-class B(A):
-    classvar1 = "I am in class B"
-
-    def __init__(self):
+        # -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # assert: assert statement in python is used to check condition and return an assertion error if the statement is false.
+        assert quantity >= 0, f"The quantity can't be less than zero" # we can also add messages to assertion error
+        # -------------------------------------------------------------------------------------------------------------------------------------------------------------------
         
-        self.var1 = "I am inside class B's constructor"
-        self.classvar1 = "Instance var in class B"
-        super().__init__()
-        print(super().classvar1)
+        
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    # creating a method to find out total value of the each good
+    def ValueCalculator(self):
+        return self.price * self.quantity
+    
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+# creating objects of this class
+
+good1 = Goods('phone', 5000, -5)
+good2 = Goods('Laptop', 30000, 2)
 
 
-a = A()
-b = B()
-
-print( b.var1)
-# print( b.classvar1)
+print(good1.ValueCalculator())
+print(good2.ValueCalculator())
