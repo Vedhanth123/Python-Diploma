0b0477465688fd4676b5042a60a9f765fb00f39d
Vedhanth <gvedhanth123@outlook.com>
2022-05-21 16:39:28 +0530

    Create ProfitCalculator.py

---
+++ ProfitCalculator.py
@@ -0,0 +1,29 @@
+principle = 10000
+roi = 0.18
+
+reinvest = [principle]
+return_ = 0
+
+withdrawable = 0
+
+
+for x in range(30):
+    
+    if(x < 8):
+        return_ = principle * roi
+        principle += return_
+        reinvest.append(return_)
+    if(x >= 8 and x < 21):
+        principle -= reinvest[abs(8-x)]
+        return_ = principle * roi
+        principle += return_
+        reinvest.append(return_)
+    if(x >= 22):
+        principle -= reinvest[abs(8-x)]
+        return_ = principle * roi
+        withdrawable += return_
+        reinvest.append(return_)
+
+
+        
+        
